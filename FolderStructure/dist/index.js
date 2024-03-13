6aef9875b2c0e55fd20ad5cde453b2be606cc412
Rahul-repositry <yadavrahul95011@gmail.com>
2024-03-13 15:19:06 +0530

    datatypes with variables and functions

--- FolderStructure/dist/index.js
+++ FolderStructure/dist/index.js
@@ -1,3 +1,23 @@
 "use strict";
-const a = "voo";
-console.log("a".concat(a)); // voovo
+// How to declare datatype in typescript
+let surname = "smith";
+let secondname = "smithy";
+let thirdname = "smithyie"; // on hover we can get the datatype
+//Union dataypes
+let Unionsurname; // it can be both number and string
+Unionsurname = 2343;
+Unionsurname = "string";
+///function
+const func = (n, m) => {
+    console.log(n, m);
+    return n * m;
+};
+const func2 = (n, m) => {
+    console.log(n, m);
+    return String(n * m);
+}; // func2  is returning a string instead of number by automatically detecting the return no need to write explicitly
+let aliaName = "John";
+const alia = (n, m) => {
+    console.log(n, m);
+    return n + m;
+}; // make funciton simple and more freindly to read.
--- FolderStructure/src/index.ts
+++ FolderStructure/src/index.ts
@@ -1,3 +1,38 @@
-const a = "voo";
+// How to declare datatype in typescript
 
-console.log("a".concat(a)); // voovo
+let surname: string = "smith";
+let secondname = <string>"smithy";
+let thirdname = "smithyie"; // on hover we can get the datatype
+
+//Union dataypes
+
+let Unionsurname: string | number; // it can be both number and string
+Unionsurname = 2343;
+Unionsurname = "string";
+
+///function
+
+const func = (n: number, m: number): number => {
+  console.log(n, m);
+  return n * m;
+};
+
+const func2 = (n: number, m: number) => {
+  console.log(n, m);
+  return String(n * m);
+}; // func2  is returning a string instead of number by automatically detecting the return no need to write explicitly
+
+//Type aliases
+
+type aliasSurname = string | Number; // add more datatypes
+
+let aliaName: aliasSurname = "John";
+
+//In funciton
+
+type aliaFunc = (n: string, m: string) => string;
+
+const alia: aliaFunc = (n, m) => {
+  console.log(n, m);
+  return n + m;
+}; // make funciton simple and more freindly to read.
