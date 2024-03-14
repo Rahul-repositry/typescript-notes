e634edc34a3309043aa31f511964006da2b55485
Rahul-repositry <yadavrahul95011@gmail.com>
2024-03-14 15:12:08 +0530

    type assertion

--- FolderStructure/dist/index.js
+++ FolderStructure/dist/index.js
@@ -3,20 +3,3 @@
 const btn = document.getElementById("btn");
 const btn1 = document.getElementById("btn");
 const btn2 = document.getElementById("btn");
-const img = document.querySelector("img"); //HTMLImage
-const form = document.querySelector("form"); //HTMLForm
-const myInput = document.querySelector("form > input");
-console.log(form);
-form.onsubmit = (e) => {
-    e.preventDefault();
-    let value = Number(myInput.value);
-    let h1 = document.createElement("h1");
-    h1.textContent = String(value);
-    let button = document.createElement("button");
-    button.textContent = "Delete";
-    button.onclick = (e) => {
-        h1.remove();
-    };
-    h1.append(button);
-    document.body.appendChild(h1);
-};
