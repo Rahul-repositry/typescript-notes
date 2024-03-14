a68ba2d75365ffad35135489aa99b79478f49eab
Rahul-repositry <yadavrahul95011@gmail.com>
2024-03-14 15:13:03 +0530

    type of keys

--- FolderStructure/dist/index.js
+++ FolderStructure/dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+//Type assertion
 const myobj = {
     name: "Abhi ",
     email: "abhi",
@@ -10,6 +11,9 @@ const getEmail = () => {
     return myobj.email;
 };
 const getData = (key) => {
+    return myobj["name"];
+};
+const getData2 = (key /* name | email (all key of Person will appear here so user can pass only person keys) */) => {
     return myobj["name"];
 };
 console.log(myobj.name);
