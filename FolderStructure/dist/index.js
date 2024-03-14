a46fc6f95e680435300830a978cbbb270a815eec
Rahul-repositry <yadavrahul95011@gmail.com>
2024-03-14 14:03:30 +0530

    Dom Updation

--- FolderStructure/dist/index.js
+++ FolderStructure/dist/index.js
@@ -1,20 +1,22 @@
 "use strict";
-//classes
-class Player {
-    constructor(height, weight, age) {
-        this.height = height;
-        this.weight = weight;
-        this.age = age;
-        this.id = String(Math.random());
-    }
-}
-class Player2 extends Player {
-    constructor(height, weight, age, special) {
-        super(height, weight, age);
-        this.special = special;
-    }
-}
-const abhi = new Player(200, 75, 34);
-const abhi2 = new Player2(200, 75, 34, true);
-console.log(abhi2);
-console.log(abhi);
+//Type Assertion
+const btn = document.getElementById("btn");
+const btn1 = document.getElementById("btn");
+const btn2 = document.getElementById("btn");
+const img = document.querySelector("img"); //HTMLImage
+const form = document.querySelector("form"); //HTMLForm
+const myInput = document.querySelector("form > input");
+console.log(form);
+form.onsubmit = (e) => {
+    e.preventDefault();
+    let value = Number(myInput.value);
+    let h1 = document.createElement("h1");
+    h1.textContent = String(value);
+    let button = document.createElement("button");
+    button.textContent = "Delete";
+    button.onclick = (e) => {
+        h1.remove();
+    };
+    h1.append(button);
+    document.body.appendChild(h1);
+};
--- FolderStructure/src/index.ts
+++ FolderStructure/src/index.ts
@@ -1,51 +1,24 @@
-//classes
+//Type Assertion
 
-class Player {
-  readonly id: string;
-  private height: number; //cannot access anywhere other than this class
-  protected weight: number; // can access in extends
-  age: number; // default public
+const btn = document.getElementById("btn") as HTMLButtonElement;
+const btn1 = <HTMLElement>document.getElementById("btn");
+const btn2 = document.getElementById("btn")!;
 
-  constructor(height: number, weight: number, age: number) {
-    this.height = height;
-    this.weight = weight;
-    this.age = age;
-    this.id = String(Math.random());
-  }
-}
+const img = document.querySelector("img") as HTMLImageElement; //HTMLImage
 
-class Player2 extends Player {
-  special: boolean;
-  constructor(height: number, weight: number, age: number, special: boolean) {
-    super(height, weight, age);
-    this.special = special;
-  }
-}
-
-const abhi = new Player(200, 75, 34);
-const abhi2 = new Player2(200, 75, 34, true);
-console.log(abhi2);
-console.log(abhi);
-
-//Now classes with interface and implemnts
-
-interface ProductType {
-  name: string;
-  price: number;
-  stock: number;
-  id: string;
-  offer?: boolean;
-}
-
-class Product implements ProductType {
-  public name: string;
-  public price: number;
-  public stock: number;
-  public id: string = String(Math.random() * 1000);
-
-  constructor(name: string, price: number, stock: number) {
-    this.name = name;
-    this.price = price;
-    this.stock = stock;
-  }
-}
+const form = document.querySelector("form") as HTMLFormElement; //HTMLForm
+const myInput = document.querySelector("form > input") as HTMLInputElement;
+console.log(form);
+form.onsubmit = (e) => {
+  e.preventDefault();
+  let value = Number(myInput.value);
+  let h1 = document.createElement("h1");
+  h1.textContent = String(value);
+  let button = document.createElement("button");
+  button.textContent = "Delete";
+  button.onclick = (e) => {
+    h1.remove();
+  };
+  h1.append(button);
+  document.body.appendChild(h1);
+};
